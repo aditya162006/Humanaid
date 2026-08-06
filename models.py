1db8fb854eaa57261b7ee414b22641a14ec23294
Aditya Mishra <adityakumarmishra162006@gmail.com>
2026-08-06 19:33:56 +0000

    Fri, Aug 7, 2026, 1:03 AM +05:30

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
-from sqlalchemy.orm import String, Integer, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 
 class Base(DeclarativeBase):
     pass
+
 class Crisis(Base):
     __tablename__ = "crises"
     id: Mapped[int] = mapped_column(Integer,primary_key= True, autoincrement=True)
@@ -11,3 +12,11 @@ class Crisis(Base):
     country: Mapped[str] = mapped_column(String, nullable=False)
     category: Mapped[str] = mapped_column(String, nullable=False)
     search_query: Mapped[str] = mapped_column(String, nullable=False)
+    
+class DonationLink(Base):
+    __tablename__ = "donation_links"
+    id: Mapped[int] = mapped_column(Integer,primary_key=True,autoincrement=True)
+    organization: Mapped[str] = mapped_column(String,nullable=False)
+    url: Mapped[str] = mapped_column(String,nullable=False)
+    crisis_id: Mapped[int] = mapped_column(ForeignKey("crises.id"),nullable=False)
+
