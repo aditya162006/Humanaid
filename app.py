39b7b1a236b299a3a31cf2c90499a1ec7f84a43d
Aditya Mishra <adityakumarmishra162006@gmail.com>
2026-08-08 12:26:54 +0000

    Sat, Aug 8, 2026, 5:56 PM +05:30

--- app.py
+++ app.py
@@ -195,7 +195,6 @@ def add_entry():
         category = request.form.get("category")
         search_query = request.form.get("search_query")
         
-
         with Session(engine) as db_session:
             CRISIS = Crisis(slang=slang, title=title, country=country,category=category,search_query=search_query)
             db_session.add(CRISIS)
