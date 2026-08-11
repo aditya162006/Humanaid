3f2446afbe9f207dfc9d980c8d44a4fece9621fe
Aditya Mishra <adityakumarmishra162006@gmail.com>
2026-08-11 09:25:25 +0000

    Tue, Aug 11, 2026, 2:55 PM +05:30

--- app.py
+++ app.py
@@ -150,8 +150,8 @@ def remove_entry():
     if not session.get('is_admin'):
         return redirect(url_for("login_admin"))
     with Session(engine) as db_session:
-        Entry = Crisis.query.all()
-    return render_template("admin_remove_entry.html", Entry=Entry)
+        entries = db_session.query(Crisis).all()
+    return render_template("admin_remove_entry.html", Entry=entries)
 
 @app.route("/logout")
 def logout():
