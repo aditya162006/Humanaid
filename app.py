90ac72338de7cd28721ba9817ca5226442999475
Aditya Mishra <adityakumarmishra162006@gmail.com>
2026-08-11 16:19:04 +0000

    Tue, Aug 11, 2026, 9:49 PM +05:30

--- app.py
+++ app.py
@@ -142,8 +142,9 @@ def add_entry():
 def edit_entry():
     if not session.get('is_admin'):
         return redirect(url_for("login_admin"))
-
-    return render_template("admin_edit_entry.html")
+    with Session(engine) as db_session:
+        entries = db_session.query(Crisis).all()
+    return render_template("admin_edit_entry.html", Entry=entries)
 
 # Remove Entry Route
 @app.route('/admin/remove_entry', methods=['GET', 'POST'])
