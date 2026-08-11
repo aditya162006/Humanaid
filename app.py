8d40bc972624eec2fe90f0245efd22698ade2907
Aditya Mishra <adityakumarmishra162006@gmail.com>
2026-08-11 09:45:58 +0000

    Tue, Aug 11, 2026, 3:15 PM +05:30

--- app.py
+++ app.py
@@ -158,7 +158,8 @@ def remove_entry():
             if crisis is None:
                 return apology("Crisis not found")
             db_session.delete(crisis)
-            db_session.commit()
+            db_session.flush()
+            
         flash("Crisis removed successfully.", "success")
         return redirect(url_for("admin_panel"))
 
