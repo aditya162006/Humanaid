e597f5a3cb47f7d62efea0baa7827e0b82d7f48b
Aditya Mishra <adityakumarmishra162006@gmail.com>
2026-08-11 09:47:47 +0000

    Tue, Aug 11, 2026, 3:17 PM +05:30

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ class Crisis(Base):
     country: Mapped[str] = mapped_column(String,nullable=False)
     category: Mapped[str] = mapped_column(String,nullable=False)
     search_query: Mapped[str] = mapped_column(String,nullable=False)
-    donation_links: Mapped[list["DonationLink"]] = relationship(back_populates="crisis")
+    donation_links: Mapped[list["DonationLink"]] = relationship(back_populates="crisis", cascade="all, delete-orphan")
 
 class DonationLink(Base):
     __tablename__ = "donation_links"
