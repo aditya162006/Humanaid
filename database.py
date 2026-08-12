490275e698d90d8a5f79e3ad98c1fbdba9bc4ca6
Aditya Mishra <adityakumarmishra162006@gmail.com>
2026-08-12 16:02:17 +0000

    Wed, Aug 12, 2026, 9:32 PM +05:30

--- database.py
+++ database.py
@@ -1,12 +1,13 @@
 import os
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 
 load_dotenv()
 
 TURSO_DATABASE_URL = os.environ.get("TURSO_DATABASE_URL")
 TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
-
+if not TURSO_DATABASE_URL or not TURSO_AUTH_TOKEN:
+    raise RuntimeError("Turso credentials not set in environment!")
 engine = create_engine(
     f"sqlite+{TURSO_DATABASE_URL}?secure=true",
     connect_args={
