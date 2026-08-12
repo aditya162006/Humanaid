e08b4f742516756fa6809a7f8cf83ae14028c0e5
Aditya Mishra <adityakumarmishra162006@gmail.com>
2026-08-12 16:18:15 +0000

    Wed, Aug 12, 2026, 9:48 PM +05:30

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #inside the module "flask" import the class "Flask"
-from flask import Flask, render_template, jsonify,request,redirect, render_template,url_for, flash,session
+from flask import Flask, render_template, jsonify,request,redirect,url_for, flash,session
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import Session, selectinload
 from sqlalchemy import select
