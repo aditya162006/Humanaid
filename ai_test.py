3639839344d3136c5184c2e28776a155b9d69c1d
Aditya Mishra <adityakumarmishra162006@gmail.com>
2026-08-12 17:43:11 +0000

    Wed, Aug 12, 2026, 11:13 PM +05:30

--- ai_test.py
+++ ai_test.py
@@ -3,9 +3,14 @@ from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
 
 response = client.models.generate_content(
-    model= "gemini-2.5-flash",
-    contents="Explain About World War 2 in 200 words.")
+    model="gemini-2.5-flash",
+    contents="Explain the humanitarian crisis in Sudan in 100 words."
+)
+
 print(response.text)
